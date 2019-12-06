5a8ac4696d8f842054d1c6dc4dd63b948cd72f55
Ed <ed.lozano.developer@gmail.com>
2019-12-05 19:16:05 -0600

    Implementing Zuul

---
+++ src/main/java/com/eduardo/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.eduardo.microservices.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy			// To enable Zuul server
+@EnableDiscoveryClient		// To enable Eureka client
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+}
